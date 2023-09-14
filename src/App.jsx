4c6bae16e85f45dc420da3485dc7eae789227d19
Percide07 <percidehonhonou@gmail.com>
2023-09-14 11:48:06 +0100

    Adding images to every card on the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='container'>
+        <div className='content'>
+            <div>Home</div>
+            <div>About React</div>
+            <div>Project</div>
+        </div>
+        <div className="navBar"> 
+            <div></div>
+            <button className='btn'>Search</button>
+        </div>
+      </header>
+      <section>
+    
+        <div className='crdSect'>
+              <div>
+                <h2>Card1</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quam, minus cupiditate vel voluptatum tempora alias perspiciatis commodi perferendis omnis, facilis aut libero quisquam molestiae totam qui modi facere maxime?</p>
+              </div>
+              <div>
+                <h2>Card2</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quam, minus cupiditate vel voluptatum tempora alias perspiciatis commodi perferendis omnis, facilis aut libero quisquam molestiae totam qui modi facere maxime?</p>
+              </div>
+              <div>
+                <h2>Card3</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quam, minus cupiditate vel voluptatum tempora alias perspiciatis commodi perferendis omnis, facilis aut libero quisquam molestiae totam qui modi facere maxime?</p>
+              </div>
+        </div>
+      </section>
     </>
   )
 }
